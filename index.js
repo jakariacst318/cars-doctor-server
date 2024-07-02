d55016e91b47b5512f07050059134a2a7aa42f04
jakariacst318 <jsjakariacst@gmail.com>
2024-07-02 15:31:56 +0600

    card delete

--- index.js
+++ index.js
@@ -47,20 +47,37 @@ async function run() {
 
             // je ghulo us / dekhaite cai kom data dekhate option us kora hoi 
             const options = {
-                projection: {  title: 1, price: 1, services_id: 1 },
+                projection: { title: 1, price: 1, services_id: 1, img: 1 },
             };
 
             const result = await servicesCollection.findOne(query, options);
             res.send(result)
         })
 
-         // book information POST
-         app.post('/bookings', async (req, res) =>{
+
+        // sum data read
+        app.get('/bookings', async (req, res) => {
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result)
+        })
+        // book information POST
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking)
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
-         })
+        })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
